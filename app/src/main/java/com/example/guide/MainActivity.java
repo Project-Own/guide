ed88a869ae564a6ddf1442104ed00feb0bca6ccf
Nirajan9860 <prajapatinirajan0@gmail.com>
2019-09-28 15:36:01 +0545

    euta bata arko

--- app/src/main/java/com/example/guide/MainActivity.java
+++ app/src/main/java/com/example/guide/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.guide;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -8,10 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private Button btn;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMapsActivity();
+            }
+        });
     }
+    public void openMapsActivity(){
+        Intent intent=new Intent(this,MapsActivity.class);
+        startActivity(intent);
+    }
+
 
 }
--- app/src/main/java/com/example/guide/MapsActivity.java
+++ app/src/main/java/com/example/guide/MapsActivity.java
@@ -40,8 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng bhaktapur = new LatLng(27.678889, 85.411077);
+        mMap.addMarker(new MarkerOptions().position(bhaktapur).title("Marker in bhaktapur"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bhaktapur,10f ));
     }
 }
