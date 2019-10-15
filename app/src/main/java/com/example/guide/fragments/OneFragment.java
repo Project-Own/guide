d7826cc7cb17c118498db7cc7de8eb5d059ed68f
rohitjoey <35337226+rohitjoey@users.noreply.github.com>
2019-10-15 16:21:15 +0545

    main screen design done

--- app/src/main/java/com/example/guide/fragments/OneFragment.java
+++ app/src/main/java/com/example/guide/fragments/OneFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +16,7 @@ import com.example.guide.R;
  */
 public class OneFragment extends Fragment {
     private View view;
+    private Button nav;
     public OneFragment() {
                 // Required empty public constructor
     }
