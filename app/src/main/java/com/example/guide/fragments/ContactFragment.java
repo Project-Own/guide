87f91ad2429f47813f85d4e6bc226ea3c9cad7cf
Nirjal-PC <nirjalprajapati@gmail.com>
2019-10-27 10:07:02 +0545

    embassies update

--- app/src/main/java/com/example/guide/fragments/ContactFragment.java
+++ app/src/main/java/com/example/guide/fragments/ContactFragment.java
@@ -42,15 +42,39 @@ public class ContactFragment extends Fragment {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
         recycleView.setLayoutManager(mLayoutManager);
         recycleView.setAdapter(adapter);
-        contacts.add(new Contact("chinese Embassy","01-4440286"));
-        contacts.add(new Contact("Indian Embassy","01-4410900"));
-        contacts.add(new Contact("Australian Embassy","01-4371678"));
-        contacts.add(new Contact("Us Embassy","01-4234000"));
-        contacts.add(new Contact("German Embassy","01-4217200"));
-        contacts.add(new Contact("France Embassy","01-4412332"));
-        contacts.add(new Contact("Korean Embassy","01-4270172"));
-        contacts.add(new Contact("Saudi Embassy","01-4720891"));
-        contacts.add(new Contact("Japan Embassy","01-4426680"));
+
+        contacts.add(new Contact("Embassy of Australia","01-4371678"));
+        contacts.add(new Contact("Embassy of Bangladesh","01-4390132"));
+        contacts.add(new Contact("Embassy of Brazil","01-4721462 /01-4721463"));
+        contacts.add(new Contact("Embassy of the People's Republic of China","01-4440286"));
+        contacts.add(new Contact("Embassy of Egypt","01-5590166"));
+        contacts.add(new Contact("Embassy of Finland","01-4417221/01-4416636"));
+        contacts.add(new Contact("Embassy of France","01-4412332/01-4414734"));
+        contacts.add(new Contact("Embassy of Germany","01-4412786/01-4416527"));
+        contacts.add(new Contact("Embassy of India","01-4410900"));
+        contacts.add(new Contact("Embassy of Israel","01-4411811/01-4413419"));
+        contacts.add(new Contact("Embassy of Japan","01-4426680"));
+
+        contacts.add(new Contact("Embassy of Malaysia","01-5545680/01-5545681"));
+        contacts.add(new Contact("Embassy of Myanmar","01- 5592774/01-5592841"));
+        contacts.add(new Contact("Embassy of the Democratic People's Republic of Korea","01- 5521855/01-5535871"));
+        contacts.add(new Contact("Embassy of Norway","01- 554 5307"));
+        contacts.add(new Contact("Embassy of Pakistan","01-4374024/01-4374016"));
+        contacts.add(new Contact("Embassy of Qatar","01-5173161"));
+        contacts.add(new Contact("Embassy of Russia","01-4412155/01-441 1063"));
+        contacts.add(new Contact("Embassy of Saudi Arabia","01-4720891"));
+        contacts.add(new Contact("Embassy of the Republic of Korea","01-4270172/01-4270417"));
+        contacts.add(new Contact("Embassy of Srilanka","01-4721389"));
+        contacts.add(new Contact("Embassy of Switzerland","01-4217008/01-5525358"));
+        contacts.add(new Contact("Embassy of Thailand","01-4371410/01-4371411"));
+        contacts.add(new Contact("Embassy of the United Arab Emirates","01-4237100"));
+        contacts.add(new Contact("Embassy of the United Kingdom","01-4371410/01-4371411"));
+        contacts.add(new Contact("Embassy the United States of America","01-4234000"));
+
+
+
+
+
         return view;
 
     }
