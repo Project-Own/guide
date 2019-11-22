660bbeb82213ba8859738c307da3bf26173844b1
Nirajan9860 <prajapatinirajan0@gmail.com>
2019-11-22 20:30:22 +0545

    dubai eutai ma

--- app/src/main/java/com/example/guide/ui/review/ReviewFragment.java
+++ app/src/main/java/com/example/guide/ui/review/ReviewFragment.java
@@ -1,10 +1,12 @@
 package com.example.guide.ui.review;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
@@ -14,12 +16,18 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.guide.R;
+import com.firebase.client.Firebase;
 
 public class ReviewFragment extends Fragment {
+    private Firebase firebase;
 
     private ReviewViewModel mViewModel;
     RatingBar ratingBar;
     Button button;
+    EditText name;
+    EditText email;
+    EditText review;
+    EditText getReview;
 
     public static ReviewFragment newInstance() {
         return new ReviewFragment();
@@ -28,9 +36,16 @@ public class ReviewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.review_fragment, container, false);
-        ratingBar= (RatingBar) view.findViewById(R.id.rating);
+        View view = inflater.inflate(R.layout.review_layout, container, false);
+
+        name=view.findViewById(R.id.editText);
+        email=view.findViewById(R.id.editText3);
+        review=view.findViewById(R.id.editText4);
+        getReview=view.findViewById(R.id.editText5);
         button= view.findViewById(R.id.button);
+        Firebase.setAndroidContext(getContext());
+        String uniqueID= Settings.Secure.getString(getActivity().getContentResolver(),Settings.Secure.ANDROID_ID);
+        firebase=new Firebase("https://review-ff940.firebaseio.com/Users"+ uniqueID);
 
         return view;
     }
@@ -39,22 +54,76 @@ public class ReviewFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Toast.makeText(getContext(),"Stars: ", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+//            @Override
+//            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+//                Toast.makeText(getContext(),"Stars: ", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(),"Stars: "+ ratingBar.getRating(), Toast.LENGTH_SHORT).show();
+
+
+                submitfeedback();
             }
         });
 
         mViewModel = ViewModelProviders.of(this).get(ReviewViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    public void submitfeedback(){
+        String username=name.getText().toString();
+        String useremail=email.getText().toString();
+        String userreview=review.getText().toString();
+        String  userreview1=getReview.getText().toString();
+        Firebase reusername=firebase.child("name");
+        reusername.setValue(username);
+        if(username.isEmpty()){
+            name.setError("this field is required");
+            button.setEnabled(false);
+        }
+        else{
+            name.setError(null);
+            button.setEnabled(true);
+        }
+        Firebase reuseremail=firebase.child("email");
+        reuseremail.setValue(useremail);
+        if(useremail.isEmpty()){
+            email.setError("this field is required");
+            button.setEnabled(false);
+        }
+        else{
+            email.setError(null);
+            button.setEnabled(true);
+        }
+        Firebase reuserreview=firebase.child("review on bhaktapur");
+        reuserreview.setValue(userreview);
+        if(userreview.isEmpty()){
+            review.setError("this field is required");
+            button.setEnabled(false);
+        }
+        else{
+            review.setError(null);
+            button.setEnabled(true);
+        }
+        Firebase reuserreview1=firebase.child("review on app");
+        reuserreview1.setValue(userreview1);
+        if(userreview1.isEmpty()){
+            getReview.setError("this field is required");
+            button.setEnabled(false);
+        }
+        else{
+            getReview.setError(null);
+            button.setEnabled(true);
+        }
+
+
+
+
+
     }
 
 
